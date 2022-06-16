3a1af18e2ce87d0c1066f40dff87cd47d48e63fb
Renzlawr <RSLogan23@gmail.com>
2022-06-16 11:34:17 -0600

    Finished, working on extras

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+    validates :title, :content, presence: true
+    validates :title, :content, uniqueness: true
+    validates :title, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'blog#index'
   get 'blogs' => 'blog#index', as: 'home'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
-  get 'blogs/:id' => 'blog#show', as: 'blog'
   get 'blogs/:id/edit' => 'blog#edit', as: 'edit_blog'
+  get 'blogs/:id' => 'blog#show', as: 'blog'
+  patch 'blogs/:id' => 'blog#update'
   post 'blogs' => 'blog#create'
   delete 'blogs/:id' => 'blog#destroy', as: 'delete_blog'
 end
